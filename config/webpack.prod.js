bb0bdc838bd480610a6ae278447e8065e67949e2
xun <2042204285@qq.com>
2023-01-05 19:03:18 +0800

    moment

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,11 +1,29 @@
 /**
  * @type {import('webpack').Configuration}
  */
-const { IS_PRODUCTION } = require("./util/constants");
+const { PUBLIC_PATH } = require("./util/constants");
 const webpackCommonConfig = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const copyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(webpackCommonConfig, {
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+    new copyWebpackPlugin({
+      patterns: [
+        {
+          from: PUBLIC_PATH,
+          to: "./",
+          globOptions: {
+            dot: true,
+            gitignore: true,
+            ignore: ["**/index.html*"],
+          },
+        },
+      ],
+    }),
+  ],
 });
