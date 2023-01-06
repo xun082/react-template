3b424f7aa731fce1f6c9a0ce6e55d253cb5bbc57
xun <2042204285@qq.com>
2023-01-06 08:46:21 +0800

    添加了一系列的babel插件

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
-const plugins = [[require("babel-plugin-await-add-trycatch")]];
+const plugins = [
+  [require("babel-plugin-await-add-trycatch")],
+  ["@babel/plugin-transform-runtime"],
+  ["@babel/plugin-transform-modules-commonjs"],
+  ["@babel/plugin-syntax-dynamic-import"],
+];
 
 module.exports = {
   presets: [
@@ -15,5 +20,18 @@ module.exports = {
   compact: true,
   // 魔法注释,用于分包
   comments: true,
-  plugins: process.env.NODE_ENV === "production" ? [...plugins] : plugins,
+  plugins:
+    process.env.NODE_ENV === "production"
+      ? [
+          ...plugins,
+          [
+            "transform-remove-console",
+            {
+              exclude: ["error", "warn"],
+              env: "production",
+              commentWords: ["no remove"],
+            },
+          ],
+        ]
+      : plugins,
 };
