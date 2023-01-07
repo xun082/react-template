8b46f1f39c9ab723b9c47ab019b21cab781feade
xun <2042204285@qq.com>
2023-01-07 18:28:26 +0800

    优化了一些开发体验

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -28,6 +28,10 @@ const devWebpackConfig = merge(
       hot: true,
       compress: true, // 是否启用 gzip 压缩
       historyApiFallback: true, // 解决前端路由刷新404现象
+      client: {
+        logging: "info",
+        overlay: false,
+      },
     },
 
     plugins: [
