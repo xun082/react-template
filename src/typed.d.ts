be428957dc2448b5605c9fed4a5058e6af7979c9
xun <2042204285@qq.com>
2023-01-30 11:58:33 +0800

    配置tsconfig

--- src/typed.d.ts
+++ src/typed.d.ts
@@ -12,3 +12,8 @@ declare module "*.module.scss" {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
